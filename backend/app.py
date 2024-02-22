505087f8282aa1116e2b143efea6bcc6e1c58b5b
LeserraJ <leserrabusiness@gmail.com>
2024-02-22 20:47:40 +0000

    sortve understanding flask routing

--- backend/app.py
+++ backend/app.py
@@ -9,4 +9,14 @@ app = Flask(__name__)
 def index():
     return render_template('index.html', title = 'Test')
 
+
+
+@app.route("/usa")
+def usa_main():
+    return render_template('usa_main.html', title = "USA Ground")
+
+
+@app.route("/germany")
+def germany_main():
+    return render_template('germany_main.html', title = "Germany Ground")
 # For hot reload CLI Command flask --app app --debug run
