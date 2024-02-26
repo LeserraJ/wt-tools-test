1b801a619b2c9a68a917daf511fb3830567d39a2
LeserraJ <leserrabusiness@gmail.com>
2024-02-26 22:00:20 +0000

    added sub folder

--- backend/app.py
+++ backend/app.py
@@ -13,10 +13,10 @@ def index():
 
 @app.route("/usa")
 def usa_main():
-    return render_template('usa_main.html', title = "USA Ground")
+    return render_template('USA/usa_main.html', title = "USA Ground")
 
 
 @app.route("/germany")
 def germany_main():
-    return render_template('germany_main.html', title = "Germany Ground")
+    return render_template('Germany/germany_main.html', title = "Germany Ground")
 # For hot reload CLI Command flask --app app --debug run
